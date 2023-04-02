83c96970eb7bfe2a2df72bd776fa93664595283a
Simonas Narbutas <simonas@narbuto.lt>
2023-04-02 23:25:26 +0300

    allow user select nodeId

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use dialoguer::{theme::ColorfulTheme, Select};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::process::Command;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -28,10 +30,37 @@ fn main() {
         .expect("failed executing ec2command");
     let nodes: Nodes =
         serde_json::from_slice(&get_nodes_cmd.stdout).expect("failed to Deserialize");
-    let rez: Vec<String> = nodes
+    let nodes: Vec<String> = nodes
         .items
         .iter()
         .map(|v| v.spec.provider_id.clone())
         .collect();
-    println!("{:#?}", rez);
+    let mut nodes_with_regions = HashMap::new();
+
+    for s in nodes {
+        let (node_id, node_region) = s
+            .split('/')
+            .nth(4)
+            .zip(s.split('/').nth(3))
+            .map(|(a, b)| (a.to_string(), b.to_string()))
+            .unwrap();
+        nodes_with_regions.insert(node_id, node_region);
+    }
+    println!("{:#?}", nodes_with_regions);
+    let node_keys = nodes_with_regions.keys().cloned().collect::<Vec<String>>();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select node to ssh to")
+        .default(0)
+        .items(&node_keys[..])
+        .interact()
+        .unwrap()
+        .to_string();
+
+    let someding = node_keys.get(selection.parse::<usize>().unwrap());
+    println!(
+        "You're about to ssh somewhere in {}!",
+        nodes_with_regions
+            .get(&someding.unwrap().to_string())
+            .unwrap()
+    );
 }
