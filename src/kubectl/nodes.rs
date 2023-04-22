f473b072d8a119178ce960b5c034d3cd0b2c1428
Simonas Narbutas <simonas@narbuto.lt>
2023-04-23 00:10:50 +0300

    refactor code for node ssh subcommand

--- src/kubectl/nodes.rs
+++ src/kubectl/nodes.rs
@@ -1,4 +1,5 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use dialoguer::{theme::ColorfulTheme, Select};
 use serde::{Deserialize, Serialize};
 use std::process::Command;
 
@@ -21,8 +22,14 @@ pub struct Spec {
     pub provider_id: String,
 }
 
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub node_id: String,
+    pub region: String,
+}
+
 impl Nodes {
-    pub fn get_nodes() -> Result<Nodes> {
+    pub fn get() -> Result<Self> {
         let kubectl_nodes = Command::new("sh")
             .arg("-c")
             .arg("kubectl get nodes -ojson")
@@ -31,4 +38,51 @@ impl Nodes {
         let nodes = serde_json::from_slice(&kubectl_nodes.stdout)?;
         Ok(nodes)
     }
+    pub fn get_providers(self) -> Result<Vec<String>> {
+        let providers: Vec<String> = self
+            .items
+            .iter()
+            .map(|n| n.spec.provider_id.clone())
+            .collect();
+        Ok(providers)
+    }
+}
+
+impl std::fmt::Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Node: {} in {}", self.node_id, self.region)
+    }
+}
+
+pub fn generate_node_ssh_cmd() -> Result<()> {
+    let node_providers: Vec<String> = Nodes::get()?.get_providers()?;
+    let mut nodes: Vec<Node> = Vec::new();
+    for s in node_providers {
+        let (node_id, region) = s
+            .split('/')
+            .nth(4)
+            .zip(s.split('/').nth(3))
+            .map(|(a, b)| (a.to_string(), b.to_string()))
+            .unwrap();
+        nodes.push(Node { node_id, region });
+    }
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select node to ssh to")
+        .default(0)
+        .items(&nodes)
+        .interact()?
+        .to_string()
+        .parse::<usize>()?;
+
+    let node_id = nodes[selection].node_id.clone();
+    let region = nodes[selection].region.clone();
+    let last_char = region.chars().last().unwrap();
+    let region_trimmed = if last_char.is_alphabetic() {
+        region.trim_end_matches(last_char)
+    } else {
+        region.as_str()
+    };
+    println!("aws ssm start-session --region {region_trimmed} --target {node_id} ");
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use dialoguer::{theme::ColorfulTheme, Select};
-use std::collections::HashMap;
 
 mod kubectl;
-use kubectl::nodes::Nodes;
+use kubectl::nodes;
 
 /// A tool to pregenerate some commands for k8s
 #[derive(Parser)]
@@ -17,50 +15,20 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Generates node ssh command
-    Node { ssh: Option<String> },
+    /// Composes node ssh command
+    Node { ssh: String },
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    let nodes = Nodes::get_nodes()?;
-    let nodes: Vec<String> = nodes
-        .items
-        .iter()
-        .map(|v| v.spec.provider_id.clone())
-        .collect();
-
-    let mut nodes_with_regions = HashMap::new();
-    for s in nodes {
-        let (node_id, node_region) = s
-            .split('/')
-            .nth(4)
-            .zip(s.split('/').nth(3))
-            .map(|(a, b)| (a.to_string(), b.to_string()))
-            .unwrap();
-        nodes_with_regions.insert(node_id, node_region);
-    }
-    let node_keys = nodes_with_regions.keys().cloned().collect::<Vec<String>>();
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select node to ssh to")
-        .default(0)
-        .items(&node_keys[..])
-        .interact()?
-        .to_string();
-
-    let someding = node_keys.get(selection.parse::<usize>()?).unwrap();
-    let region = nodes_with_regions.get(&someding.to_string()).unwrap();
-    let last_char = region.chars().last().unwrap();
-    let region_trimmed = if last_char.is_alphabetic() {
-        region.trim_end_matches(last_char)
-    } else {
-        region
-    };
+    //    match &cli.command {
+    //        Commands::Node { .. } => {
+    //            println!("aws ssm start-session --region {region_trimmed} --target {someding} ")
+    //        }
+    //    }
     match &cli.command {
-        Commands::Node { .. } => {
-            println!("aws ssm start-session --region {region_trimmed} --target {someding} ")
-        }
-    }
+        Commands::Node { .. } => nodes::generate_node_ssh_cmd()?,
+    };
     Ok(())
 }
